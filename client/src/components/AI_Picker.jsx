51518b73a0fcd94bdf1cb5c89b6029a45007a7a9
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-05-06 15:17:10 +0530

    ✨ Components files

---
+++ client/src/components/AI_Picker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AI_Picker = () => {
+  return (
+    <div>AI_Picker</div>
+  )
+}
+
+export default AI_Picker
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,13 @@
 import CustomButton from './CustomButton'
+import ColorPicker from './ColorPicker'
+import Tab from './Tab'
+import FilePicker from './FilePicker'
+import AI_Picker from './AI_Picker'
 
-export { CustomButton };
+export {
+  CustomButton,
+  ColorPicker,
+  Tab,
+  FilePicker,
+  AI_Picker
+};
