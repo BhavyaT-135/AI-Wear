c7f83cff2a84090d238cb00d88cac223b78d4826
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-05-11 01:15:03 +0530

    ✨ Canvas and Shirt Model

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -7,7 +7,18 @@ import CameraRig from './CameraRig'
 
 const CanvasModel = () => {
   return (
-    <div>CanvasModel</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      
+      {/* <CameraRig>
+        <Backdrop> */}
+          <Center>
+            <Shirt />
+          </Center>
+        {/* </Backdrop>
+      </CameraRig> */}
+    </Canvas>
   )
 }
 
