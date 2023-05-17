5afc65f6371cb94f0e1615e577e1d53107567609
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-05-17 13:23:00 +0530

    ✨ Buttons configuration switch case

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -14,6 +14,33 @@ const Customizer = () => {
 
   const snap = useSnapshot(state);
 
+  const [file, setFile] = useState('')
+
+  const [prompt, setPrompt] = useState('')
+
+  const [genratingImg, setGenratingImg] = useState(false)
+
+  const [activeEditorTab, setActiveEditorTab] = useState('')
+
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false
+  })
+
+  // Show the tab content depending on the activeTab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+      case "filepicker":
+        return <FilePicker />
+      case "aipicker":
+        return <AI_Picker />
+      default:
+        return null;
+    }
+  }
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -29,9 +56,11 @@ const Customizer = () => {
                   <Tab 
                     key={tab.name}
                     tab={tab}
-                    handleClick={() => { }}
+                    handleClick={() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+
+                {generateTabContent()}  
               </div>
             </div>
           </motion.div>
